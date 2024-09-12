601fa0084d6f6d021f7699291ae3b52f43453c1b
Kelvyn Rodrigues <168141936+Kelvyn2707@users.noreply.github.com>
2024-09-12 12:34:15 -0300

    Design Patterns Proxy

---
+++ Design_Patter_Proxy/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import model.Orcamento;
+import model.OrcamentoProxy;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main( String[] args ) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"), new BigDecimal(10));
+        System.out.println("Valor do orçamento = " + orcamento.getValorOrcamento());
+        System.out.println("Tempo para ver o desconto = " + orcamento.getDescontoOrcamento());
+
+        OrcamentoProxy proxy = new OrcamentoProxy(orcamento);
+        System.out.println("Valor sem proxy = " + proxy.getDescontoOrcamento());
+        System.out.println("Valor com proxy = " + proxy.getDescontoOrcamento());
+        System.out.println("Valor com proxy = " + proxy.getDescontoOrcamento());
+        System.out.println("Valor com proxy = " + proxy.getDescontoOrcamento());
+        System.out.println("Valor com proxy = " + proxy.getDescontoOrcamento());
+    }
+}
